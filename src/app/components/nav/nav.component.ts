78f1ad92707569fab40d81bb10f11f309c0a5d34
pedroscosta9 <A037160@ismai.pt>
2026-04-14 14:34:39 +0100

    Navbar mobile

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -6,13 +6,22 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.scss' 
+  styleUrl: './nav.component.scss'
 })
 export class NavComponent {
   isScrolled = false;
+  isMenuOpen = false;
 
   @HostListener('window:scroll')
   onScroll() {
     this.isScrolled = window.scrollY > 60;
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
   }
 }
